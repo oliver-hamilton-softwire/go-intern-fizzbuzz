23a539aac80ddfb6c875f10d64a7aaa9c457f17c
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-08 14:42:36 +0100

    Adds multiple of 13 functionality

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -20,6 +21,25 @@ func fizzbuzz(num int) string {
 	if num%11 == 0 {
 		output = []string{"Bong"}
 	}
+	// Add Fezz immediately in front of the first thing beginning with B
+	if num%13 == 0 {
+		var fezzAdded = false
+		for i, word := range output {
+			if word[0] == 'B' {
+				//temp := slices.(output[:i], "Fezz")
+				//fmt.Println(temp)
+				//fmt.Println(output)
+				output = slices.Concat(output[:i], []string{"Fezz"}, output[i:])
+				//fmt.Println(output)
+				fezzAdded = true
+				break
+			}
+		}
+		if !fezzAdded {
+			output = append(output, "Fezz")
+			fezzAdded = true
+		}
+	}
 	if len(output) == 0 {
 		output = []string{strconv.Itoa(num)}
 	}
@@ -28,7 +48,7 @@ func fizzbuzz(num int) string {
 
 // This is our main function, this executes by default when we run the main package.
 func main() {
-	for i := 1; i <= 100; i++ {
+	for i := 11 * 13; i <= 11*13; i++ {
 		fmt.Println(fizzbuzz(i))
 	}
 }
