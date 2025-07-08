53a39ad089c0953e48628862e934ed8d0b33d6c9
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-08 16:14:21 +0100

    Adds tests for when using command-line arguments

--- main_test.go
+++ main_test.go
@@ -7,6 +7,12 @@ type Test struct {
 	expected string
 }
 
+type TestWithOptions struct {
+	num             int
+	commandLineArgs []string
+	expected        string
+}
+
 var tests = []Test{
 	{6, "Fizz"},
 	{10, "Buzz"},
@@ -16,9 +22,25 @@ var tests = []Test{
 	{3 * 5 * 17, "BuzzFizz"},
 }
 
+var testsWithOptions = []TestWithOptions{
+	{3 * 5 * 17, []string{"3"}, "Fizz"},
+	{3 * 5 * 17, []string{"5"}, "Buzz"},
+	{3 * 5 * 17, []string{"3", "5"}, "FizzBuzz"},
+	{3 * 5 * 17, []string{"3", "5", "17"}, "BuzzFizz"},
+}
+
 func TestFizzbuzz(t *testing.T) {
 	for _, test := range tests {
 		result := fizzbuzz(test.num, []string{})
+		if result != test.expected {
+			t.Errorf("incorrect output for num %d got: %s expected: %s", test.num, result, test.expected)
+		}
+	}
+}
+
+func TestFizzbuzzWithOptions(t *testing.T) {
+	for _, test := range testsWithOptions {
+		result := fizzbuzz(test.num, test.commandLineArgs)
 		if result != test.expected {
 			t.Errorf("incorrect output for num %d got: %s expected: %s", test.num, result, test.expected)
 		}
