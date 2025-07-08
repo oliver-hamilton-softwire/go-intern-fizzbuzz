3d82be39dda4d8138ba0f66dc3b07bfa88673881
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-08 14:48:16 +0100

    Adds tests for current functionality

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+type Test struct {
+	num      int
+	expected string
+}
+
+var tests = []Test{
+	{6, "Fizz"},
+	{10, "Buzz"},
+	{3 * 7, "FizzBang"},
+	{3 * 11, "Bong"},
+	{11 * 13, "FezzBong"},
+}
+
+func TestFizzbuzz(t *testing.T) {
+	for _, test := range tests {
+		result := fizzbuzz(test.num)
+		if result != test.expected {
+			t.Errorf("incorrect output for num %d got: %s expected: %s", test.num, result, test.expected)
+		}
+	}
+}
